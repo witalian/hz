765bc72b2c9cd314a9bd63e7bda48cdbc2273fe8
witalian <42076981+witalian@users.noreply.github.com>
2023-01-27 23:11:35 +0200

    Оновити main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
+let t = 0
 basic.forever(function () {
+    t = input.soundLevel()
+    led.plotBarGraph(
+    t,
+    255
+    )
     if (input.buttonIsPressed(Button.A)) {
-        basic.showNumber(input.temperature())
+        basic.showNumber(t)
     }
 })
